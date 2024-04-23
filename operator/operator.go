b7572cf22b8272a9e62216d62114ab2c77949d56
qiang_liu <liuqiang9596@gmail.com>
2024-04-23 19:06:37 +0800

    case: left shift

---
+++ operator/operator.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1 << 0)
+	fmt.Println(1 << 1)
+	fmt.Println(1 << 2)
+	fmt.Println(1 << 3)
+	fmt.Println(1 << 4)
+	fmt.Println(1 << 5)
+	fmt.Println(1 << 6)
+	fmt.Println(1 << 7)
+	fmt.Println(1 << 8)
+	fmt.Println(1 << 9)
+
+	fmt.Println(1 << 10)
+	fmt.Println(1 << 11)
+	fmt.Println(1 << 12)
+	fmt.Println(1 << 13)
+	fmt.Println(1 << 14)
+	fmt.Println(1 << 15)
+	fmt.Println(1 << 16)
+	fmt.Println(1 << 17)
+	fmt.Println(1 << 18)
+
+	fmt.Println(1 << 19)
+	fmt.Println(1 << 20)
+	fmt.Println(1 << 21)
+	fmt.Println(1 << 22)
+	fmt.Println(1 << 23)
+	fmt.Println(1 << 24)
+	fmt.Println(1 << 25)
+	fmt.Println(1 << 26)
+	fmt.Println(1 << 27)
+	fmt.Println(1 << 28)
+	fmt.Println(1 << 29)
+	fmt.Println(1 << 30)
+}
