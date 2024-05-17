7e4d047465bc447ec4d44a2f16b64a15e75a423c
qiang_liu <liuqiang9596@gmail.com>
2024-05-17 22:46:41 +0800

    复数的用法

--- enum/main.go
+++ enum/main.go
@@ -8,12 +8,18 @@ const (
 	Blue, ColorMask = (1 << iota), (1 << (iota + 1)) - 1
 )
 
+const (
+	i complex128 = complex(1000, 1000)
+)
+
 func main() {
 
 	fmt.Println("Red:", Red)
 	fmt.Println("Green:", Green)
 	fmt.Println("Blue:", Blue)
 	fmt.Println("ColorMask:", ColorMask)
+
+	fmt.Println("i:", i)
 }
 
 // Output:
@@ -21,3 +27,4 @@ func main() {
 //Green: 2
 //Blue: 4
 //ColorMask: 7
+//i: (1000+1000i)
